b323530778e16e09e2cecfc3ab1a5530980cfbd6
abhishek-dgliger <abhishek.verma1@dgliger.com>
2025-12-09 15:59:54 +0530

    updates

--- host/src/App.js
+++ host/src/App.js
@@ -4,9 +4,9 @@ export default function App() {
   return (
     <>
       <h1>Loading page</h1>
-      <a href="http://localhost:3001">Products</a>
+      <a href="/products">Products</a>
       <br />
-      <a href="http://localhost:3002">Cart</a>
+      <a href="/cart">Cart</a>
     </>
   );
 }
